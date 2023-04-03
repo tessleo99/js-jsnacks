3872612692be803137116ddca4f6d5713021e208
tessleo99 <t.napoletano9@gmail.com>
2023-04-03 15:23:25 +0200

    jsnak1

---
+++ jsncks1/js/main.js
@@ -0,0 +1,23 @@
+//FAR INSERIRE ALL'UTENTE DUE NUMERI
+let numberUserChoice1 = prompt("Inserisci un numero :");
+let numberUserChoice2 = prompt("Inserisci un numero :");
+//CONVERTIRE I NUMERI DA STRINGA A NUMERI
+numberUserChoice1 = parseInt(numberUserChoice1);
+numberUserChoice2 = parseInt(numberUserChoice2);
+//CONTROLLARE  CHE L'UTENTE NON ABBIA INSERITO DELLE LETTERE 
+if (numberUserChoice1 = isNaN){
+    console.log("Errore: inserisci un numero!");
+}
+else if (numberUserChoice2 = isNaN){
+    console.log("Errore: inserisci un numero!");
+}
+//STAMPARE IL NUMERO MAGGIORE
+else if (numberUserChoice1 > numberUserChoice2){
+    console.log(numberUserChoice1);
+}
+else if (numberUserChoice1 < numberUserChoice2){
+    console.log(numberUserChoice2);
+}
+
+
+
